e1d594e5b281eb13c03e0191f35f400ae20f5080
Yash Vardhan Kejriwal <74137771+ProgrammingWithYash@users.noreply.github.com>
2025-06-04 18:08:19 +0530

    Better api and stuff

--- src/Api/Storefront/StorefrontClient.php
+++ src/Api/Storefront/StorefrontClient.php
@@ -13,4 +13,61 @@ class StorefrontClient extends BaseClient
     public function getUsers() {
         return $this->authManager->makeStorefrontRequest('/get_users.php');
     }
+
+    /**
+     * Get checkout data for a user
+     */
+    public function getCheckoutData($userId, $coupon = null) {
+        $data = ['user_id' => $userId];
+        if ($coupon) {
+            $data['coupon'] = $coupon;
+        }
+        return $this->authManager->makeStorefrontRequest('/checkout.php', 'POST', $data);
+    }
+
+    /**
+     * Finalize an order
+     */
+    public function finalizeOrder($orderData) {
+        return $this->authManager->makeStorefrontRequest('/finalize-order.php', 'POST', $orderData);
+    }
+
+    /**
+     * General method to call any storefront API endpoint
+     */
+    public function callEndpoint($endpoint, $method = 'GET', $data = null, $headers = []) {
+        return $this->authManager->makeStorefrontRequest($endpoint, $method, $data, $headers);
+    }
+
+    /**
+     * Make a GET request to any endpoint
+     */
+    public function get($endpoint, $params = null, $headers = []) {
+        $url = $endpoint;
+        if ($params) {
+            $url .= '?' . http_build_query($params);
+        }
+        return $this->authManager->makeStorefrontRequest($url, 'GET', null, $headers);
+    }
+
+    /**
+     * Make a POST request to any endpoint
+     */
+    public function post($endpoint, $data = null, $headers = []) {
+        return $this->authManager->makeStorefrontRequest($endpoint, 'POST', $data, $headers);
+    }
+
+    /**
+     * Make a PUT request to any endpoint
+     */
+    public function put($endpoint, $data = null, $headers = []) {
+        return $this->authManager->makeStorefrontRequest($endpoint, 'PUT', $data, $headers);
+    }
+
+    /**
+     * Make a DELETE request to any endpoint
+     */
+    public function delete($endpoint, $data = null, $headers = []) {
+        return $this->authManager->makeStorefrontRequest($endpoint, 'DELETE', $data, $headers);
+    }
 } 
